0e86e098817da619476f9877fe13ec0278169f91
Emma Ahmad <emmaahmadsir@gmail.com>
2025-01-06 22:37:09 +0800

    added router and view

---
+++ src/view/HeaderLogo.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header">
+      <!-- <img src="@/assets/summafy-logo.png" alt="Logo" class="logo" /> -->
+      <h1 class="title">Summafy</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderLogo'
+  };
+  </script>
+  
+  <style scoped>
+  .header {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    background-color: #f8f9fa;
+  }
+  
+  .logo {
+    height: 50px;
+    margin-right: 10px;
+  }
+  
+  .title {
+    font-size: 24px;
+    color: #333;
+  }
+  </style>
